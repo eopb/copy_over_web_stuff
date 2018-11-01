2216001b8d67327ee346436645ece2f2826d71e8
Ethan Brierley <ethanboxx@gmail.com>
2018-11-01 17:57:28 +0000

    echo out what is going on

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Remove old website files
+echo Removing old website files
 rm -r /var/www/html/*
-# Copy files form usb to the Apache folder.
+echo Copying files form usb to the Apache folder.
 cp -a /media/pi/USB\ DISK/. /var/www/html/
-# Fix broken permissions.
+echo Fixing broken permissions.
 chgrp -R www-data /var/www/html
 find /var/www/html -type d -exec chmod g+rx {} +
 find /var/www/html -type f -exec chmod g+r {} +
